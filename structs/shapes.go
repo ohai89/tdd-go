5f11fc4105f37676a58f1a4c9144c0686de2858f
Ilmari Aho <ohai89@pm.me>
2022-11-14 16:33:19 +0200

    Ran through gofmt

--- structs/shapes.go
+++ structs/shapes.go
@@ -3,7 +3,7 @@ package main
 import "math"
 
 type Shape interface {
-  Area() float64
+	Area() float64
 }
 
 type Rectangle struct {
@@ -12,14 +12,13 @@ type Rectangle struct {
 }
 
 func (r Rectangle) Area() float64 {
-  return r.Width * r.Height
+	return r.Width * r.Height
 }
 
 type Circle struct {
-  Radius float64
+	Radius float64
 }
 
 func (c Circle) Area() float64 {
-  return math.Pi * c.Radius * c.Radius
+	return math.Pi * c.Radius * c.Radius
 }
-
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -4,21 +4,21 @@ import "testing"
 
 func TestArea(t *testing.T) {
 
-  checkArea := func(t testing.TB, shape Shape, want float64) {
-    t.Helper()
-    got := shape.Area()
-    if got != want {
-      t.Errorf("got %g want %g", got, want)
-    }
-  }
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
 
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-    checkArea(t, rectangle, 72.0)
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-	  checkArea(t, circle, 314.1592653589793)
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
