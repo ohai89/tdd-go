84f17234d9386ddedc2aa5ccb60f91cbb09ab8a7
Ilmari Aho <ohai89@pm.me>
2022-11-18 16:39:20 +0200

    Wallet created

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,3 +1,16 @@
 package main
 
-type Wallet struct{}
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}
+
+func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
+	w.balance += amount
+}
