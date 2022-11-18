1972a14030cbfc5253d9f0fdbd0ce61cb06e907f
Ilmari Aho <ohai89@pm.me>
2022-11-18 17:01:02 +0200

    Added withdraw method, using common assertBalance

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -23,6 +23,10 @@ func (w *Wallet) Balance() Euro {
 
 func (w *Wallet) Deposit(amount Euro) {
 	// Need to use pointer so that balance lives at the same memory location
-	fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
+	// fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Euro) {
+	w.balance -= amount
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,23 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	wallet := Wallet{}
+	assertBalance := func(t testing.TB, wallet Wallet, want Euro) {
+		t.Helper()
+		got := wallet.Balance()
 
-	wallet.Deposit(Euro(10))
-
-	got := wallet.Balance()
-
-	fmt.Printf("address of balance in test is %v\n", &wallet.balance)
-
-	want := Euro(10)
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Euro(10))
+		assertBalance(t, wallet, Euro(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Euro(20)}
+		wallet.Withdraw(Euro(10))
+		assertBalance(t, wallet, Euro(10))
+	})
 }
