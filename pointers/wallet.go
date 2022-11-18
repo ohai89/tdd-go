eefdee453857d3cc425a98d39b261111444de7d5
Ilmari Aho <ohai89@pm.me>
2022-11-18 16:43:07 +0200

    Forgot pointers

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -6,11 +6,13 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
+	// Need to use pointer!
 	return w.balance
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
+	// Need to use pointer so that balance lives at the same memory location
 	fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
 	w.balance += amount
 }
